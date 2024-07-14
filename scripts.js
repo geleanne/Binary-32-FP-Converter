9938877738986bb48378c09d69954f24dc16fc85
Angelene Atienza <marielle_angelene_atienza@dlsu.edu.ph>
2024-07-15 00:11:43 +0800

    Update input labels and placeholders based on selected format

---
+++ scripts.js
@@ -0,0 +1,32 @@
+document.addEventListener("DOMContentLoaded", (event) => {
+    const inputType = document.getElementById('input-type');
+    const mantissaLabel = document.getElementById('mantissa-label');
+    const mantissaInput = document.getElementById('mantissa-input');
+    const exponentLabel = document.getElementById('exponent-base');
+
+    inputType.addEventListener('change', function() {
+        const selectedFormat = inputType.value;
+        let mantissaLabelText = 'Decimal';
+        let placeholderText = 'Enter a decimal number';
+        let exponentLabelText = 'Exponent (Base- 10)';
+
+        if (selectedFormat === 'binary') {
+            mantissaLabelText = 'Binary';
+            placeholderText = 'Enter a binary number';
+            exponentLabelText = 'Exponent (Base- 2)';
+        } else if (selectedFormat === 'decimal') {
+            mantissaLabelText = 'Decimal';
+            placeholderText = 'Enter a decimal number';
+            exponentLabelText = 'Exponent (Base- 10)';
+        // not yet sure, placeholder muna
+        } else if (selectedFormat === 'nan') {
+            mantissaLabelText = 'NaN';
+            placeholderText = 'Enter NaN value';
+            exponentLabelText = 'NaN';
+        }
+
+        mantissaLabel.textContent = mantissaLabelText;
+        mantissaInput.placeholder = placeholderText;
+        exponentLabel.textContent = exponentLabelText;
+    });
+});
